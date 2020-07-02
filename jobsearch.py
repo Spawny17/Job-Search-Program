9d031bc833cb3704f30a3e3549e13a1001e5a85b
Spawny17 <46385242+Spawny17@users.noreply.github.com>
2020-07-02 16:40:57 +0100

    Update jobsearch.py

--- jobsearch.py
+++ jobsearch.py
@@ -11,7 +11,7 @@ writer.writerow(['Job Title', 'Job Link'])
 
 # Define the functions for each job website
 def totalJobs():
-    website = requests.get("https://www.totaljobs.com/jobs/junior-developer/in-nr31-0lt?radius=30&postedwithin=3").text
+    website = requests.get("https://www.totaljobs.com/jobs/junior-developer/in-POST-CODE?radius=30&postedwithin=3").text
     soup = BeautifulSoup(website, 'lxml')
     for jobdiv in soup.find_all('div', class_='job-title'):
         title = jobdiv.h2.text
@@ -19,7 +19,7 @@ def totalJobs():
         writer.writerow([title, link])
 
 def indeedJobs():
-    website = requests.get("https://www.indeed.co.uk/jobs?as_and=junior+developer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&salary=&radius=25&l=Great+Yarmouth&fromage=3&limit=20&sort=date&psf=advsrch").text
+    website = requests.get("https://www.indeed.co.uk/jobs?as_and=junior+developer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&salary=&radius=25&l=LOCATION+HERE&fromage=3&limit=20&sort=date&psf=advsrch").text
     soup = BeautifulSoup(website, 'lxml')
     for jobdiv in soup.find_all('div', class_='title'):
         title = jobdiv.a.text
@@ -27,7 +27,7 @@ def indeedJobs():
         writer.writerow([title, "https://www.indeed.co.uk/" + link])
 
 def reedJobs():
-    website = requests.get("https://www.reed.co.uk/jobs/junior-developer-jobs-in-nr310lt?datecreatedoffset=LastThreeDays&proximity=30").text
+    website = requests.get("https://www.reed.co.uk/jobs/junior-developer-jobs-in-POSTCODE?datecreatedoffset=LastThreeDays&proximity=30").text
     soup = BeautifulSoup(website, 'lxml')
     for jobs in soup.find_all('article', class_='job-result'):
         title = jobs.h3.text
